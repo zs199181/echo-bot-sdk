208e273bae0406808397d3312db6400cc229ae31
zs199181 <jean50fuji@gmail.com>
2017-09-16 15:08:47 +0700

    Update chapter-01.php
    test

--- chapter-01.php
+++ chapter-01.php
@@ -8,10 +8,8 @@ $access_token  = 'HatdNCqxGq+BiYUUAxXYbz8Bjlx1Mx1O8tHLxim2y2cF2BuWxGxt6fIIfnudfQ
 $bot = new BOT_API($channelSecret, $access_token);
 	
 if (!empty($bot->isEvents)) {
-    $test = "Poom ".$bot->message['text'];
-    $text = "HI ".$test;
-		
-    $bot->replyMessageNew($bot->replyToken, $text);
+	
+    $bot->replyMessageNew($bot->replyToken, json_encode($bot));
 
     if ($bot->isSuccess()) {
         echo 'Succeeded!';
